3d3ddb43f880a3c8d2952a36d2bf4d88e7fb80a2
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2021-12-13 17:16:10 +0530

    Create cmd.py

---
+++ VampireXspam/plugins/cmd.py
@@ -0,0 +1,25 @@
+🔥 ＶＡＭＰＩＲＥ✘ＳＰＡＭ 🔥
+ 
+ ᴄᴍᴅs ᴀᴠᴀɪʟᴀʙʟᴇ ɪɴ ʀɪᴢᴏᴇʟ x sᴘᴀᴍ
+
+ 𖥔ＵＳＥＲＢＯＴ ＣＭＤＳ𖥔
+ 
+ .ping - to check ping
+ .alive - to check bot alive/version (only main userbot will reply)
+ .restart - to restart all spam bots
+ 
+ 𖥔ＬＥＡＶＥ ＣＭＤ𖥔
+ 
+ .leave - to leave public/private channel/groups
+ 
+ 𖥔ＳＰＡＭ ＣＭＤＳ𖥔
+ 
+ .raid - to raid
+ .replyraid - to active reply raid
+ .dreplyraid - to de-active reply raid 
+ .spam - this cmd use for Normal spam
+ .bigspam - this cmd use for big spam
+ .delayspam - this cmd use for delay spam
+ .uspam - this cmd use for unlimited spam
+ 
+© @TheVampireBotz | @VampireBot_Support
