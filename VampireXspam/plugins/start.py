a6e9df1d10e45397f35f0d32980d49ad25e91cc0
♡мιcкεү♡ <88245294+MickeyxD@users.noreply.github.com>
2021-12-22 00:52:46 +0530

    Update start.py

--- VampireXspam/plugins/start.py
+++ VampireXspam/plugins/start.py
@@ -3,15 +3,15 @@ import os
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon import events, Button
 from telethon.tl.custom import button
-from .. import Riz, Riz2, Riz3, Riz4, Riz5, Riz6, Riz7, Riz8, Riz9, Riz10, ALIVE_PIC
+from .. import Geo, Geo2, Geo3, Geo4, Geo5, Geo6, Geo7, Geo8, Geo9, Geo10, ALIVE_PIC
 
-RIZ_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
+GEO_IMG = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ba87c58f01a6fcb5ef512.jpg"
 
 
-Riz_Button = [
+Geo_Button = [
         [
-        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/RiZoeLX"),
-        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/DNHxHELL")
+        Button.url("ᴄʜᴀɴɴᴇʟ", "https://t.me/VampireBotz_offici"),
+        Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/Vampirebotz_support")
         ],
         [
         Button.url("• ʀᴇᴘᴏ •", "https://github.com/MrRizoel/RiZoeLXSpam")
@@ -19,31 +19,31 @@ Riz_Button = [
         ]
 
 
-@Riz.on(events.NewMessage(pattern="/start"))
+@Geo.on(events.NewMessage(pattern="/start"))
 async def start(event):              
     if event.is_private:
-       RizBot = await Riz.get_me()
-       bot_id = RizBot.first_name
-       bot_username = RizBot.username
+       GeoBot = await Geo.get_me()
+       bot_id = GeoBot.first_name
+       bot_username = GeoBot.username
        replied_user = await event.client(GetFullUserRequest(event.sender_id))
-       TheRiZoeL = event.chat_id
+       TheVampire = event.chat_id
        firstname = replied_user.user.first_name
-       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/RiZoeLX)**"
-       await Riz.send_file(TheRiZoeL,
-                RIZ_IMG,
-                caption=Rizmsg, 
-                buttons=Riz_Button)
+       Geomsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/VampireBotz_official)**"
+       await Geo.send_file(TheVampire,
+                GEO_IMG,
+                caption=Geomsg, 
+                buttons=Geo_Button)
                 
-@Riz2.on(events.NewMessage(pattern="/start"))
+@Geo2.on(events.NewMessage(pattern="/start"))
 async def start(event):              
     if event.is_private:
-       RizBot = await Riz2.get_me()
-       bot_id = RizBot.first_name
-       bot_username = RizBot.username
+       GeoBot = await Geo2.get_me()
+       bot_id = GeoBot.first_name
+       bot_username = GeoBot.username
        replied_user = await event.client(GetFullUserRequest(event.sender_id))
-       TheRiZoeL = event.chat_id
+       TheVampire = event.chat_id
        firstname = replied_user.user.first_name
-       Rizmsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/RiZoeLX)**"
+      Geomsg = f"**Hello, {firstname} ! Nice To Meet You, Well I Am {bot_id}, An Powerfull Spam Bot.** \n\n**If You Want Your Own Spam Bots You Can Deploy From Button Below.** \n\n**𝐏𝐎𝐖𝐄𝐑𝐄𝐃 𝐁𝐘 [𝐑𝐈𝐙𝐎𝐄𝐋 𝐗](https://t.me/VampireBotz_official)**"
        await Riz2.send_file(TheRiZoeL,
                 RIZ_IMG,
                 caption=Rizmsg, 
